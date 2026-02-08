933c9007ae24db354aa132ce2eeca04c70b351f4
Hyun-Bum Yang <yhyunbum@gmail.com>
2026-02-08 18:07:04 -0500

    Refactor frame reading into its own class.

---
+++ engine/include/engine/util.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+
+// Convert AVERROR num into string
+std::string AvErrToStr(int errnum);
--- engine/src/engine_api.cc
+++ engine/src/engine_api.cc
@@ -1,58 +1,45 @@
 #include "engine/engine_api.h"
 
+#include <memory>
 #include <string>
 
 #include "absl/status/status.h"
-#include "absl/strings/str_format.h"
+#include "absl/status/statusor.h"
+#include "engine/frame_reader.h"
 #include "spdlog/spdlog.h"
 
 extern "C" {
 #include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavutil/error.h>
 }  // extern "C"
 
 namespace {
 
-std::string AvErrToStr(int errnum) {
-  char errbuf[AV_ERROR_MAX_STRING_SIZE] = {0};
-  return std::string(av_make_error_string(errbuf, sizeof(errbuf), errnum));
-}
-
 // TODO: Migrate these functionalities into a separate class for better
 // testability
 absl::Status DecodeVideoInternal(const char* input_path) {
-  AVFormatContext* format_ctx = avformat_alloc_context();
-  int open_res = avformat_open_input(&format_ctx, input_path, nullptr, nullptr);
-  if (open_res != 0) {
-    return absl::Status(
-        absl::StatusCode::kInvalidArgument,
-        absl::StrFormat("Failed to open input video file %s with error: %s",
-                        input_path, AvErrToStr(open_res)));
+  absl::StatusOr<std::unique_ptr<viduce::engine::FrameReader>> frame_reader =
+      viduce::engine::FrameReader::Create(input_path);
+  if (!frame_reader.ok()) {
+    return frame_reader.status();
   }
 
-  // Read frames
-  // av_read_frame will be responsible for reaching each frames which will block
-  // until it's ready.
-  // If input is being "streamed" in, then we must not block the thread:
-  // https://stackoverflow.com/questions/23800615/ffmpeg-av-open-input-stream-in-latest-ffmpeg
-  AVPacket* packet = av_packet_alloc();
   while (true) {
-    int read_res = av_read_frame(format_ctx, packet);
-    if (read_res == AVERROR_EOF) {
-      // Done processing
+    absl::StatusOr<std::unique_ptr<viduce::engine::Frame>> frame =
+        (*frame_reader)->ReadNextFrame();
+    if (!frame.ok()) {
+      return frame.status();
+    }
+
+    if (*frame == nullptr) {
+      // Done reading
       break;
     }
 
-    if (read_res != 0) {
-      return absl::Status(absl::StatusCode::kInternal,
-                          absl::StrFormat("Error reading frame with error: %s",
-                                          AvErrToStr(read_res)));
-    }
-
-    // TODO: Process the packet
-    spdlog::info("Read packet with size: {}", packet->size);
+    AVFrame* av_frame = (*frame)->get_frame();
+    spdlog::info("Decoded frame info: res ({%d}x{%d}", av_frame->width,
+                 av_frame->height);
   }
+
   return absl::OkStatus();
 }
 
