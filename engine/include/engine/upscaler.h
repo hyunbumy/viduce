ba7c55c7c37b581698787d0c7c955b920b17f176
Hyun-Bum Yang <yhyunbum@gmail.com>
2026-03-21 21:42:10 -0400

    initial impl

--- engine/include/engine/upscaler.h
+++ engine/include/engine/upscaler.h
@@ -3,16 +3,14 @@
 #include <memory>
 
 #include "absl/status/statusor.h"
+#include "engine/frame.h"
 
 namespace viduce::engine {
-
-struct ImageFrame {};
 
 class Upscaler {
  public:
   // Returns an upscaled version of the input image frame.
-  absl::StatusOr<std::unique_ptr<ImageFrame>> Upscale(
-      const ImageFrame* input_img);
+  absl::StatusOr<std::unique_ptr<Frame>> Upscale(Frame* input_frame);
 };
 
 }  // namespace viduce::engine
--- engine/src/upscaler.cc
+++ engine/src/upscaler.cc
@@ -3,8 +3,12 @@
 #include <memory>
 #include <opencv2/dnn.hpp>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
+#include "absl/cleanup/cleanup.h"
 #include "absl/status/statusor.h"
+#include "engine/frame.h"
+#include "engine/util.h"
 #include "litert/c/litert_common.h"
 #include "litert/cc/litert_common.h"
 #include "litert/cc/litert_compiled_model.h"
@@ -12,89 +16,182 @@
 #include "litert/cc/litert_macros.h"
 #include "litert/cc/litert_model.h"
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
+}  // extern "C"
+
 namespace viduce::engine {
 
 namespace {
 
-constexpr float kBit8ScaleFactor = 1.0f / 255.0f;
-constexpr int kScale = 4;
+constexpr int kUpscale = 4;
+
+constexpr AVPixelFormat kModelIOFormat = AV_PIX_FMT_RGB24;
+constexpr int kModelIONormScale = 255;
+
+absl::Status Validate(Frame* frame) {
+  if (frame == nullptr) {
+    return absl::InvalidArgumentError("Input frame is null");
+  }
+
+  if (frame->stream_info().media_type != AVMEDIA_TYPE_VIDEO) {
+    return absl::InvalidArgumentError("Input frame is not a video frame");
+  }
+
+  if (frame->frame()->format != AV_PIX_FMT_YUV420P) {
+    return absl::InvalidArgumentError(
+        "Input frame format is not YUV420P. Only YUV420P is supported for now");
+  }
+
+  return absl::OkStatus();
+}
+
+struct ModelIO {
+  // Flattened pixel data in (ch, h, w) layout with normalized values in
+  // [0.0, 1.0]
+  std::vector<float> data;
+};
+
+absl::StatusOr<ModelIO> ToModelInput(Frame* frame) {
+  AVFrame* av_frame = frame->frame();
+
+  // Convert input frame to RGB format; for now do 8-bit
+  // TODO: Consider supporting higher img depth
+  std::unique_ptr<Frame> new_frame = Frame::Create({});
+  AVFrame* rgb_frame = new_frame->frame();
+
+  SwsContext* sws_scale_ctx = sws_getContext(
+      av_frame->width, av_frame->height, (AVPixelFormat)av_frame->format,
+      av_frame->width, av_frame->height, (AVPixelFormat)kModelIOFormat,
+      SWS_FAST_BILINEAR,
+      /*srcFilter=*/nullptr, /*dstFilter=*/nullptr, /*param=*/nullptr);
+  absl::Cleanup free_sws_ctx = [&sws_scale_ctx]() {
+    sws_freeContext(sws_scale_ctx);
+  };
+  if (sws_scale_ctx == nullptr) {
+    return absl::InternalError(
+        "Failed to create SwsContext for color conversion");
+  }
+
+  if (int res = sws_scale_frame(sws_scale_ctx, rgb_frame, av_frame); res < 0) {
+    return absl::InternalError("Failed to scale frame with error: " +
+                               AvErrToStr(res));
+  }
+
+  // Copy data into a vector in RGB: (ch, h, w) and normalize to [0.0f, 1.0f]
+  // We can iterate through directly since:
+  //   * Already in RGB
+  //   * Packed so single plane in (ch, h, w)
+  std::vector<float> normalized(rgb_frame->linesize[0]);
+  for (int i = 0; i < normalized.size(); ++i) {
+    normalized[i] = rgb_frame->data[0][i] / float(kModelIONormScale);
+  }
+  return ModelIO{.data = std::move(normalized)};
+}
+
+// Convert the model output back to an image.
+absl::StatusOr<std::unique_ptr<Frame>> FromModelOutput(Frame* input_frame,
+                                                       const ModelIO& output) {
+  // Denormalize pixel values into RGB24
+  std::vector<uint8_t> denormalized(output.data.size());
+  for (int i = 0; i < denormalized.size(); ++i) {
+    denormalized[i] = output.data[i] * kModelIONormScale;
+  }
+
+  AVFrame* input_avframe = input_frame->frame();
+  // Convert pixels to a temp frame for sws_scale
+  std::unique_ptr<Frame> temp_frame = Frame::Create({});
+  AVFrame* temp_avframe = temp_frame->frame();
+  temp_avframe->format = kModelIOFormat;
+  temp_avframe->width = input_avframe->width * kUpscale;
+  temp_avframe->height = input_avframe->height * kUpscale;
+  int size = av_image_fill_arrays(
+      temp_avframe->data, temp_avframe->linesize, denormalized.data(),
+      kModelIOFormat, temp_avframe->width, temp_avframe->height, /*align=*/1);
+
+  // Convert to a new frame in the same format as the input frame.
+  SwsContext* sws_scale_ctx = sws_getContext(
+      temp_avframe->width, temp_avframe->height,
+      (AVPixelFormat)temp_avframe->format, temp_avframe->width,
+      temp_avframe->height, (AVPixelFormat)input_avframe->format,
+      SWS_FAST_BILINEAR,
+      /*srcFilter=*/nullptr, /*dstFilter=*/nullptr, /*param=*/nullptr);
+  absl::Cleanup free_sws_ctx = [&sws_scale_ctx]() {
+    sws_freeContext(sws_scale_ctx);
+  };
+  if (sws_scale_ctx == nullptr) {
+    return absl::InternalError(
+        "Failed to create SwsContext for color conversion");
+  }
+
+  std::unique_ptr<Frame> new_frame = Frame::Create(input_frame->stream_info());
+  AVFrame* new_avframe = new_frame->frame();
+  if (int res = sws_scale_frame(sws_scale_ctx, new_avframe, temp_avframe);
+      res < 0) {
+    return absl::InternalError("Failed to scale frame with error: " +
+                               AvErrToStr(res));
+  }
+
+  return std::move(new_frame);
+}
+
+// TODO: Extract into its separate class and receive as dependency
+absl::StatusOr<litert::CompiledModel> GetModel() {
+  // Initialize LiteRT environment
+  LITERT_ASSIGN_OR_RETURN(auto env, litert::Environment::Create({}));
+
+  // Compile the model for the CPU
+  LITERT_ASSIGN_OR_RETURN(
+      litert::CompiledModel compiled_model,
+      litert::CompiledModel::Create(env, "models/output/realesrgan.tflite",
+                                    litert::HwAccelerators::kCpu));
+  return compiled_model;
+}
+
+// Runs the model and returns the output
+absl::StatusOr<ModelIO> RunModel(litert::CompiledModel& model,
+                                 const ModelIO& input) {
+  LITERT_ASSIGN_OR_RETURN(std::vector<litert::TensorBuffer> input_buffers,
+                          model.CreateInputBuffers());
+  input_buffers[0].Write<float>(absl::MakeConstSpan(input.data));
+
+  LITERT_ASSIGN_OR_RETURN(std::vector<litert::TensorBuffer> output_buffers,
+                          model.CreateOutputBuffers());
+  LITERT_RETURN_IF_ERROR(model.Run(input_buffers, output_buffers));
+
+  LITERT_ASSIGN_OR_RETURN(size_t output_buffer_size, output_buffers[0].Size());
+  size_t output_length = output_buffer_size / sizeof(float);
+  std::vector<float> output(output_length);
+  LITERT_RETURN_IF_ERROR(output_buffers[0].Read<float>(absl::MakeSpan(output)));
+
+  // Normalize the model output to [0.0f, 1.0f] in case of outliers
+  for (float& val : output) {
+    val = std::min(std::max(val, 0.0f), 1.0f);
+  }
+
+  return ModelIO{.data = std::move(output)};
+}
 
 }  // namespace
 
-absl::StatusOr<std::unique_ptr<ImageFrame>> Upscaler::Upscale(
-    const ImageFrame* input_img) {
-  // TODO: Convert input_img to cv::Mat
-  cv::Mat input;
-
-  // Convert to format that the model expects: RGB, float32, normalized to
-  // [0.0f, 1.0f], and in (ch, h, w) layout.
-  // For now, assume 8-bit BGR input.
-  cv::Mat rgb_float_chw =
-      cv::dnn::blobFromImage(input, kBit8ScaleFactor, cv::Size(), cv::Scalar(),
-                             /*swapRB=*/true, /*crop=*/false, CV_32F);
-
-  std::cout << "input img total: " << rgb_float_chw.total() << std::endl;
-
-  // Initialize LiteRT environment
-  LITERT_ASSIGN_OR_ABORT(auto env, litert::Environment::Create({}));
-
-  // Compile the model for the CPU
-  LITERT_ASSIGN_OR_ABORT(
-      auto compiled_model,
-      litert::CompiledModel::Create(env, "models/output/realesrgan.tflite",
-                                    litert::HwAccelerators::kCpu));
-
-  LITERT_ASSIGN_OR_ABORT(auto input_buffers,
-                         compiled_model.CreateInputBuffers());
-  std::cout << "Input buffers size: " << input_buffers.size() << std::endl;
-  LITERT_ASSIGN_OR_ABORT(auto input_req,
-                         compiled_model.GetInputBufferRequirements(0));
-  LITERT_ASSIGN_OR_ABORT(auto buf_size, input_req.BufferSize());
-  // With an example input of (ch=3, h=1, w=1) of type float32, the buf_size is
-  // 3 elem with 12 bytes (3 * 4 bytes for float32)
-  std::cout << "Input buffer size: " << buf_size << std::endl;
-
-  input_buffers[0].Write<float>(
-      absl::MakeConstSpan(rgb_float_chw.ptr<float>(),
-                          rgb_float_chw.total() * rgb_float_chw.channels()));
-
-  LITERT_ASSIGN_OR_RETURN(auto output_buffers,
-                          compiled_model.CreateOutputBuffers());
-  std::cout << "Output buffers size: " << output_buffers.size() << std::endl;
-  LITERT_ASSIGN_OR_ABORT(auto output_req,
-                         compiled_model.GetOutputBufferRequirements(0));
-  LITERT_ASSIGN_OR_ABORT(auto out_buf_size, output_req.BufferSize());
-  // With an example input of (ch=3, h=1, w=1) of type float32 and scale of 4,
-  // the out_buf_size is 48 elem with 192 bytes (48 * 4 bytes for float32)
-  std::cout << "Output buffer size: " << out_buf_size << std::endl;
-
-  LITERT_RETURN_IF_ERROR(compiled_model.Run(input_buffers, output_buffers));
-
-  // Convert the output weight back to an image.
-  // Output of the model should be a weight ranging [0.0f, 1.0f] for each pixel
-  // in the input image.
-  // Output layout: RGB and (batch=1, ch, h, w)
-  std::vector<float> output(out_buf_size / sizeof(float));
-  LITERT_RETURN_IF_ERROR(output_buffers[0].Read<float>(absl::MakeSpan(output)));
-
-  // Construct the output image.
-  // Denormalize the pixel values
-  for (float& val : output) {
-    val = std::min(std::max(val, 0.0f), 1.0f) * 255.0f;
+absl::StatusOr<std::unique_ptr<Frame>> Upscaler::Upscale(Frame* input_frame) {
+  if (absl::Status status = Validate(input_frame); status != absl::OkStatus()) {
+    return status;
   }
-  // Convert to image
-  int batch = 1;
-  int ch = 3;
-  int h = input.rows * kScale;
-  int w = input.cols * kScale;
-  std::vector<int> sizes{batch, ch, h, w};
-  cv::Mat out_rgb_float_chw(
-      /*ndims=*/sizes.size(), sizes.data(), CV_32F, (void*)output.data());
-  std::vector<cv::Mat> output_imgs;
-  cv::dnn::imagesFromBlob(out_rgb_float_chw, output_imgs);
-  std::cout << "Output imgs size: " << output_imgs.size() << std::endl;
 
-  return std::make_unique<ImageFrame>();
+  LITERT_ASSIGN_OR_RETURN(ModelIO input, ToModelInput(input_frame));
+
+  LITERT_ASSIGN_OR_RETURN(litert::CompiledModel compiled_model, GetModel());
+
+  LITERT_ASSIGN_OR_RETURN(ModelIO output, RunModel(compiled_model, input));
+
+  LITERT_ASSIGN_OR_RETURN(std::unique_ptr<Frame> new_frame,
+                          FromModelOutput(input_frame, output));
+
+  return new_frame;
 }
 
 }  // namespace viduce::engine
